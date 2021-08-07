7700ff94c9a0cdf1ebe29faba58740b42de3a91d
nganhvu <anhvu2001ct@gmail.com>
2021-08-07 23:06:23 +0700

    .

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,14 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const cssnano = require('cssnano');
-const rename = require('gulp-rename');
 const prefix = require('autoprefixer');
 const fileinclude = require('gulp-file-include');
 const browserSync = require('browser-sync').create();
 
-const project = "WED201c";
+const project = "index";
 const srcPath = `src/${project}/`;
-const distPath = (project == "index") ? "dist/" : `project/${project}/`;
+const distPath = "dist/" + (project == "index" ? "index/" : `project/${project}/`);
+console.log(distPath)
 
 function allFiles(dir, ext, extFolder=true) {
   if (!extFolder) return `${dir}**/*.${ext}`;
@@ -25,12 +25,11 @@ function createPages() {
 }
 
 function styling() {
-  const plugins = [prefix(), cssnano()];
   return gulp.src(allFiles(srcPath, "scss"))
     .pipe(sass().on("error", sass.logError))
+    .pipe(postcss([prefix()]))
     .pipe(gulp.dest(srcPath+"css"))
-    .pipe(postcss(plugins))
-    .pipe(rename(path => {path.basename += ".min"}))
+    .pipe(postcss([cssnano()]))
     .pipe(gulp.dest(distPath+"css"))
     .pipe(browserSync.stream());
 }
