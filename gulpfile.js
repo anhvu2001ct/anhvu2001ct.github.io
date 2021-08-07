75cb5a9ea7367cf5436345da40a4ed26dc457955
nganhvu <anhvu2001ct@gmail.com>
2021-08-07 14:29:16 +0700

    .

--- gulpfile.js
+++ gulpfile.js
@@ -2,29 +2,36 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const cssnano = require('cssnano');
-const rename = require("gulp-rename");
+const rename = require('gulp-rename');
 const prefix = require('autoprefixer');
 const browserSync = require('browser-sync').create();
 
-const path_scss = "./src/scss/**/*.scss";
+const project = "WED201c";
+const srcPath = `src/${project}/`;
+const distPath = `dist/${project}/`;
+
+function allFiles(dir, ext, extFolder=true) {
+  if (!extFolder) return `${dir}**/*.${ext}`;
+  return `${dir}${ext}/**/*.${ext}`
+}
 
 function styling() {
   const plugins = [prefix(), cssnano()];
-  return gulp.src(path_scss)
+  return gulp.src(allFiles(srcPath, "scss"))
     .pipe(sass().on("error", sass.logError))
-    .pipe(gulp.dest("./src/css"))
+    .pipe(gulp.dest(srcPath+"css"))
     .pipe(postcss(plugins))
     .pipe(rename(path => {path.basename += ".min"}))
-    .pipe(gulp.dest("./dist/css"))
+    .pipe(gulp.dest(distPath+"css"))
     .pipe(browserSync.stream());
 }
 
 function watch() {
   browserSync.init({
-    server: {baseDir: "dist"}
+    server: {baseDir: distPath}
   });
-  gulp.watch(path_scss, styling);
-  gulp.watch(["dist/**/*.html", "dist/**/*.js"]).on("change", browserSync.reload);
+  gulp.watch(allFiles(srcPath, "scss"), styling);
+  gulp.watch([allFiles(distPath, "html", false), allFiles(distPath, "js")]).on("change", browserSync.reload);
 }
 
 exports.style = styling;
